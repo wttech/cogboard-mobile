74450c79dcb2b57f48e772bd2b2bb68dd5f47aa0
Piotr Mazurek <piotr.mazurek98@gmail.com>
2020-10-29 23:29:45 +0100

    shared preferences hotfix

--- lib/providers/config_provider.dart
+++ lib/providers/config_provider.dart
@@ -9,7 +9,6 @@ import 'package:cogboardmobileapp/utils/shared_preferences_utils.dart';
 import 'package:enum_to_string/enum_to_string.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
 
 class ConfigProvider with ChangeNotifier {
   Config _config;
@@ -29,7 +28,9 @@ class ConfigProvider with ChangeNotifier {
   }
 
   void everyMinuteCheckTimer(Timer timer) async {
-    await checkIfQuarantineExpirationDateHasExceeded();
+    if(_urlPreferences!=null) {
+      await checkIfQuarantineExpirationDateHasExceeded();
+    }
   }
 
   Future<void> checkIfQuarantineExpirationDateHasExceeded() async {
