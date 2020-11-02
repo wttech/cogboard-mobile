2bc82363995d28ca952c0976a34a32a5bbe384d1
unknown <michal.r.adamski@student.put.poznan.pl>
2020-11-02 22:28:08 +0100

    COG-34-add-text-widget

--- lib/widgets/widgets/text/text_widget.dart
+++ lib/widgets/widgets/text/text_widget.dart
@@ -1,7 +1,5 @@
-import 'package:cogboardmobileapp/models/link_list_model.dart';
 import 'package:cogboardmobileapp/models/widget_model.dart';
 import 'package:cogboardmobileapp/widgets/widgets/details_container.dart';
-import 'package:cogboardmobileapp/widgets/widgets/link_list/link_list_item.dart';
 import 'package:flutter/material.dart';
 
 class TextWidget extends StatelessWidget {
@@ -12,25 +10,30 @@ class TextWidget extends StatelessWidget {
   });
 
   String get getText {
-    return widget.content["text"];
+    return widget.content['text'];
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return DetailsContainer(
       children: [
         Expanded(
           child: Container(
-            margin: const EdgeInsets.fromLTRB(35.0, 25.0, 35.0, 25.0),
-            child: SingleChildScrollView(
-              child: Text(
-                getText,
-                style: TextStyle(
-                  fontSize: 20,
+            margin: const EdgeInsets.fromLTRB(0, 25.0, 0, 25.0),
+            child: ListView(
+              children: [
+                Container(
+                  margin: const EdgeInsets.fromLTRB(30.0, 0, 0, 0),
+                  child: Text(
+                    getText,
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
-          )
+          ),
         ),
       ],
     );
