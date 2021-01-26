1723784ed126f83857ce33f08d100ca321c9460b
szymon.owczarzak <szymon.owczarzak@cognifide.com>
2021-01-26 10:48:56 +0100

    localizations map reverted so it has language first

--- lib/translations/app_localizations.dart
+++ lib/translations/app_localizations.dart
@@ -10,300 +10,109 @@ class AppLocalizations {
   }
 
   static Map<String, Map<String, String>> _localizedValues = {
-    'loginScreen.connect': {
-      'en': 'CONNECT',
-    },
-    'loginScreen.add.new.connection': {
-      'en': 'ADD NEW CONNECTION',
-    },
-    'addConnectionScreen.title': {
-      'en': 'New Connection',
-    },
-    'addConnectionScreen.name': {
-      'en': 'Name',
-    },
-    'addConnectionScreen.name.emptyError': {
-      'en': 'This field is required',
-    },
-    'addConnectionScreen.name.duplicateError': {
-      'en': 'This connection name is occupied.',
-    },
-    'addConnectionScreen.url': {
-      'en': 'Url',
-    },
-    'addConnectionScreen.url.emptyError': {
-      'en': 'This field is required',
-    },
-    'addConnectionScreen.url.duplicateError': {
-      'en': 'This connection name is occupied.',
-    },
-    'addConnectionScreen.url.validationError': {
-      'en': 'Url is not valid.',
-    },
-    'addConnectionScreen.addConnection': {
-      'en': 'ADD CONNECTION',
-    },
-    'urlSelect.noConnections': {
-      'en': 'Welcome. Start by adding a connection.',
-    },
-    'dashboardsProvider.Home': {
-      'en': 'Dashboard',
-    },
-    'dashboardsProvider.Favourites': {
-      'en': 'Favourites',
-    },
-    'dashboardsProvider.Quarantine': {
-      'en': 'Quarantine',
-    },
-    'dashboardsScreen.boardError.title': {
-      'en': 'Board error',
-    },
-    'dashboardsScreen.boardError.apiError.body': {
-      'en': 'API connection error occurred!',
-    },
-    'dashboardsScreen.boardError.websocketError.body': {
-      'en': 'Websocket connection error occurred!',
-    },
-    'dashboardsScreen.widgetChangedNotification': {
-      'en': 'Some widgets have changed their status:',
-    },
-    'dashboardsScreen.hintDialogTextRefreshFetchConfig': {
-      'en': 'Swipe up to fetch config again',
-    },
-    'dashboardsScreen.hintDialogTextSwipeBoards': {
-      'en': 'Swipe left and right to switch between boards',
-    },
-    'dashboardsScreenBottomNavigationBar.Home': {
-      'en': 'Home',
-    },
-    'dashboardsScreenBottomNavigationBar.Favourites': {
-      'en': 'Favourites',
-    },
-    'dashboardsScreenBottomNavigationBar.Quarantine': {
-      'en': 'Quarantine',
-    },
-    'dashboardsScreen.hintDialogConfirm': {
-      'en': 'Ok',
-    },
-    'emptyWidgetList.body': {
-      'en': 'This list is empty.',
-    },
-    'settingsHints.hints': {
-      'en': 'Hints',
-    },
-    'settingsHints.showHints': {
-      'en': 'Show hints',
-    },
-    'settingsNotifications.notifications': {
-      'en': 'Notifications',
-    },
-    'settingsNotifications.showNotifications': {
-      'en': 'Show notifications',
-    },
-    'settingsNotifications.notificationsFrequencyInput': {
-      'en': 'Show notifications',
-    },
-    'settingsProjectList.projects': {
-      'en': 'Projects',
-    },
-    'settingsScreen.title': {
-      'en': 'Settings',
-    },
-    'settingsSortBy.widgets': {
-      'en': 'Widgets',
-    },
-    'settingsSortBy.sortBy': {
-      'en': 'Sort by',
-    },
-    'settingsSortBy.none': {
-      'en': 'NONE',
-    },
-    'settingsSortBy.name': {
-      'en': 'NAME',
-    },
-    'settingsSortBy.status': {
-      'en': 'STATUS',
-    },
-    'widgetScreen.errorTitle': {
-      'en': 'Widget details',
-    },
-    'widgetScreen.hintDialogText': {
-      'en': 'Swipe left and right to switch between widget details',
-    },
-    'widgetScreen.hintDialogConfirm': {
-      'en': 'Ok',
-    },
-    'widgetScreen.alertDialog.message': {
-      'en': 'How do you want to add a widget to quarantine(With or without expiration date)?',
-    },
-    'widgetScreen.alertDialog.cancel': {
-      'en': 'CANCEL',
-    },
-    'widgetScreen.alertDialog.withExpirationDate': {
-      'en': 'WITH',
-    },
-    'widgetScreen.alertDialog.withoutExpirationDate': {
-      'en': 'WITHOUT',
-    },
-    'widgetListScreen.errorTitle': {
-      'en': 'Dashboard',
-    },
-    'widgetListErrorScreen.retry': {
-      'en': 'RETRY',
-    },
-    'widgetListScreen.errorBody': {
-      'en': 'Websocket connection error occurred!',
-    },
-    'widgetListScreen.hintDialogText': {
-      'en': 'Swipe left to remove widget from list',
-    },
-    'widgetListScreen.hintDialogConfirm': {
-      'en': 'Ok',
-    },
-    'homeScreen.viewContent': {
-      'en': 'VIEW CONTENT',
-    },
-    'aemBundleInfo.excludedBundles': {
-      'en': 'Excluded bundles',
-    },
-    'aemBundleInfo.noExcludedBundles': {
-      'en': 'No excluded bundles',
-    },
-    'aemBundleInfo.inactiveBundles': {
-      'en': 'Inactive bundles',
-    },
-    'aemBundleInfo.noInactiveBundles': {
-      'en': 'No inactive bundles',
-    },
-    'aemHealthcheck.healthchecks': {
-      'en': 'Healthchecks',
-    },
-    'bambooDeployment.details': {
-      'en': 'Details',
-    },
-    'bambooDeployment.deploymentState': {
-      'en': 'Deployment state: ',
-    },
-    'bambooDeployment.lifecycleState': {
-      'en': 'Lifecycle state: ',
-    },
-    'bambooPlan.details': {
-      'en': 'Details',
-    },
-    'bambooPlan.state': {
-      'en': 'State: ',
-    },
-    'jenkinsJob.details': {
-      'en': 'Details',
-    },
-    'jiraBuckets.bucket': {
-      'en': 'Bucket',
-    },
-    'jiraBuckets.issues': {
-      'en': 'Issues',
-    },
-    'jiraBuckets.noBuckets': {
-      'en': 'No buckets available',
-    },
-    'sonarQube.details': {
-      'en': 'Details',
-    },
-    'widgetStatus.errorConfiguration': {
-      'en': 'Configuration Error',
-    },
-    'worldClock.loading': {
-      'en': 'Loading...',
-    },
-    'dissmisibleWidgetList.removed': {
-      'en': 'Removed ',
-    },
-    'dissmisibleWidgetList.undo': {
-      'en': 'UNDO',
-    },
-    'serviceCheck.response': {
-      'en': 'Response',
-    },
-    'urlLauncher.toast': {
-      'en': 'URL error occurred',
-    },
-    'iframeEmbed.blankUrl': {
-      'en': 'URL is blank',
-    },
-    'zabbix.history': {
-      'en': 'HISTORICAL DATA',
-    },
-    'zabbix.date': {
-      'en': 'Date',
-    },
-    'widget.notUpdated': {
-      'en': 'Not updated yet',
-    },
-    'settingsScreen.projects': {
-      'en': 'Projects',
-    },
-    'settingsScreen.general': {
-      'en': 'General',
-    },
-    'settingsProjectsScreen.title': {
-      'en': 'Projects',
-    },
-    'settingsProjectsScreen.alert.title': {
-      'en': 'Are you sure?',
-    },
-    'settingsProjectsScreen.alert.confirm': {
-      'en': 'YES',
-    },
-    'settingsProjectsScreen.alert.decline': {
-      'en': 'NO',
-    },
-    'settingsGeneralScreen.title': {
-      'en': 'General',
-    },
-    'settingsGeneralScreen.widgetSorting': {
-      'en': 'Widgets Sorting',
-    },
-    'settingsGeneralScreen.notifications': {
-      'en': 'Notifications',
-    },
-    'settingsGeneralScreen.notificationFrequency': {
-      'en': 'Notification Frequency',
-    },
-    'settingsGeneralScreen.hints': {
-      'en': 'Hints',
-    },
-    'settingsGeneralScreen.alert.confirm': {
-      'en': 'OK',
-    },
-    'settingsSortByWidget.sortBy': {
-      'en': 'Sort By',
-    },
-    'settingsSortByWidget.none': {
-      'en': 'None',
-    },
-    'settingsSortByWidget.name': {
-      'en': 'Name',
-    },
-    'settingsSortByWidget.status': {
-      'en': 'Status',
-    },
-    'settingsSortByWidget.order': {
-      'en': 'Order',
-    },
-    'settingsSortByWidget.asc': {
-      'en': 'Ascending',
-    },
-    'settingsSortByWidget.desc': {
-      'en': 'Descending',
-    },
-    'addConnectionScreen.title.edit': {
-      'en': 'Edit Connection',
-    },
-    'addConnectionScreen.save': {
-      'en': 'SAVE',
-    },
+    'en': {
+      'loginScreen.connect': 'CONNECT',
+      'loginScreen.add.new.connection': 'ADD NEW CONNECTION',
+      'addConnectionScreen.title': 'New Connection',
+      'addConnectionScreen.name': 'Name',
+      'addConnectionScreen.name.emptyError': 'This field is required',
+      'addConnectionScreen.name.duplicateError': 'This connection name is occupied.',
+      'addConnectionScreen.url': 'Url',
+      'addConnectionScreen.url.emptyError': 'This field is required',
+      'addConnectionScreen.url.duplicateError': 'This connection name is occupied.',
+      'addConnectionScreen.url.validationError': 'Url is not valid.',
+      'addConnectionScreen.addConnection': 'ADD CONNECTION',
+      'urlSelect.noConnections': 'Welcome. Start by adding a connection.',
+      'dashboardsProvider.Home': 'Dashboard',
+      'dashboardsProvider.Favourites': 'Favourites',
+      'dashboardsProvider.Quarantine': 'Quarantine',
+      'dashboardsScreen.boardError.title': 'Board error',
+      'dashboardsScreen.boardError.apiError.body': 'API connection error occurred!',
+      'dashboardsScreen.boardError.websocketError.body': 'Websocket connection error occurred!',
+      'dashboardsScreen.widgetChangedNotification': 'Some widgets have changed their status:',
+      'dashboardsScreen.hintDialogTextRefreshFetchConfig': 'Swipe up to fetch config again',
+      'dashboardsScreen.hintDialogTextSwipeBoards': 'Swipe left and right to switch between boards',
+      'dashboardsScreenBottomNavigationBar.Home': 'Home',
+      'dashboardsScreenBottomNavigationBar.Favourites': 'Favourites',
+      'dashboardsScreenBottomNavigationBar.Quarantine': 'Quarantine',
+      'dashboardsScreen.hintDialogConfirm': 'Ok',
+      'emptyWidgetList.body': 'This list is empty.',
+      'settingsHints.hints': 'Hints',
+      'settingsHints.showHints': 'Show hints',
+      'settingsNotifications.notifications': 'Notifications',
+      'settingsNotifications.showNotifications': 'Show notifications',
+      'settingsNotifications.notificationsFrequencyInput': 'Show notifications',
+      'settingsProjectList.projects': 'Projects',
+      'settingsScreen.title': 'Settings',
+      'settingsSortBy.widgets': 'Widgets',
+      'settingsSortBy.sortBy': 'Sort by',
+      'settingsSortBy.none': 'NONE',
+      'settingsSortBy.name': 'NAME',
+      'settingsSortBy.status': 'STATUS',
+      'widgetScreen.errorTitle': 'Widget details',
+      'widgetScreen.hintDialogText': 'Swipe left and right to switch between widget details',
+      'widgetScreen.hintDialogConfirm': 'Ok',
+      'widgetScreen.alertDialog.message':
+          'How do you want to add a widget to quarantine(With or without expiration date)?',
+      'widgetScreen.alertDialog.cancel': 'CANCEL',
+      'widgetScreen.alertDialog.withExpirationDate': 'WITH',
+      'widgetScreen.alertDialog.withoutExpirationDate': 'WITHOUT',
+      'widgetListScreen.errorTitle': 'Dashboard',
+      'widgetListErrorScreen.retry': 'RETRY',
+      'widgetListScreen.errorBody': 'Websocket connection error occurred!',
+      'widgetListScreen.hintDialogText': 'Swipe left to remove widget from list',
+      'widgetListScreen.hintDialogConfirm': 'Ok',
+      'homeScreen.viewContent': 'VIEW CONTENT',
+      'aemBundleInfo.excludedBundles': 'Excluded bundles',
+      'aemBundleInfo.noExcludedBundles': 'No excluded bundles',
+      'aemBundleInfo.inactiveBundles': 'Inactive bundles',
+      'aemBundleInfo.noInactiveBundles': 'No inactive bundles',
+      'aemHealthcheck.healthchecks': 'Healthchecks',
+      'bambooDeployment.details': 'Details',
+      'bambooDeployment.deploymentState': 'Deployment state: ',
+      'bambooDeployment.lifecycleState': 'Lifecycle state: ',
+      'bambooPlan.details': 'Details',
+      'bambooPlan.state': 'State: ',
+      'jenkinsJob.details': 'Details',
+      'jiraBuckets.bucket': 'Bucket',
+      'jiraBuckets.issues': 'Issues',
+      'jiraBuckets.noBuckets': 'No buckets available',
+      'sonarQube.details': 'Details',
+      'widgetStatus.errorConfiguration': 'Configuration Error',
+      'worldClock.loading': 'Loading...',
+      'dissmisibleWidgetList.removed': 'Removed ',
+      'dissmisibleWidgetList.undo': 'UNDO',
+      'serviceCheck.response': 'Response',
+      'urlLauncher.toast': 'URL error occurred',
+      'iframeEmbed.blankUrl': 'URL is blank',
+      'zabbix.history': 'HISTORICAL DATA',
+      'zabbix.date': 'Date',
+      'widget.notUpdated': 'Not updated yet',
+      'settingsScreen.projects': 'Projects',
+      'settingsScreen.general': 'General',
+      'settingsProjectsScreen.title': 'Projects',
+      'settingsProjectsScreen.alert.title': 'Are you sure?',
+      'settingsProjectsScreen.alert.confirm': 'YES',
+      'settingsProjectsScreen.alert.decline': 'NO',
+      'settingsGeneralScreen.title': 'General',
+      'settingsGeneralScreen.widgetSorting': 'Widgets Sorting',
+      'settingsGeneralScreen.notifications': 'Notifications',
+      'settingsGeneralScreen.notificationFrequency': 'Notification Frequency',
+      'settingsGeneralScreen.hints': 'Hints',
+      'settingsGeneralScreen.alert.confirm': 'OK',
+      'settingsSortByWidget.sortBy': 'Sort By',
+      'settingsSortByWidget.none': 'None',
+      'settingsSortByWidget.name': 'Name',
+      'settingsSortByWidget.status': 'Status',
+      'settingsSortByWidget.order': 'Order',
+      'settingsSortByWidget.asc': 'Ascending',
+      'settingsSortByWidget.desc': 'Descending',
+      'addConnectionScreen.title.edit': 'Edit Connection',
+      'addConnectionScreen.save': 'SAVE'
+    }
   };
 
   String getTranslation(String key) {
-    return _localizedValues[key][locale.languageCode];
+    return _localizedValues[locale.languageCode][key];
   }
 }
